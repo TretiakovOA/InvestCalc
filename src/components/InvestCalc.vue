bf276dddd7789f9efc853cf9a5d7129ef955f51e
tretiakovoa <tretiakovoa@yandex.ru>
2019-10-27 21:29:50 +0300

    Begin

---
+++ src/components/InvestCalc.vue
@@ -0,0 +1,39 @@
+<template>    
+    <b-container>
+        <h1>Расчет эффективности инвестиций</h1>
+        <b-form @submit="onSubmit" @reset="onReset">
+            <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                    <b-button block href="#" v-b-toggle.accordion-1 variant="info">Периоды расчета</b-button>
+                </b-card-header>
+                <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">                    
+                    <b-card-body>  
+                        <b-card-text class="left">Выберите продолжительность одного периода:</b-card-text>                      
+                        <b-form-select v-model="selectedTime" :options="times"></b-form-select>
+
+                    </b-card-body>
+                </b-collapse>
+            </b-card>
+        </b-form>
+    </b-container>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selectedTime: 'year',
+            times: [                
+                { value: 'year', text: 'год' },
+                { value: 'halfyear', text: 'полугодие' },
+                { value: 'quarter', text: 'квартал' },
+                { value: 'month', text: 'месяц' }
+            ]
+        }
+    }
+}
+</script>
+<style>
+    .left {
+        text-align: left;
+    }
+</style>
