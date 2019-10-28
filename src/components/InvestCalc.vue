0ceeae2fa95163b9de2717a8f5d1b9a35299f188
Олег Александрович Третьяков <o.tretyakov@lengu.local>
2019-10-28 16:02:31 +0300

    28-10-2019

--- src/components/InvestCalc.vue
+++ src/components/InvestCalc.vue
@@ -4,13 +4,29 @@
         <b-form @submit="onSubmit" @reset="onReset">
             <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block href="#" v-b-toggle.accordion-1 variant="info">Периоды расчета</b-button>
+                    <b-button block href="#" v-b-toggle.accordion-1 variant="primary">Периоды расчета</b-button>
                 </b-card-header>
-                <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">                    
+                <b-collapse id="accordion-1" role="tabpanel" visible="true">                    
                     <b-card-body>  
                         <b-card-text class="left">Выберите продолжительность одного периода:</b-card-text>                      
-                        <b-form-select v-model="selectedTime" :options="times"></b-form-select>
-
+                        <b-form-select v-model="selectedTime" :options="times"></b-form-select>                        
+                        <b-card-text class="left">Введите общее количество периодов:</b-card-text>
+                        <b-form-input v-model="numPeriods" type="number" min="0" max="1000"></b-form-input>
+                    </b-card-body>
+                </b-collapse>
+            </b-card>
+            <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                    <b-button block href="#" v-b-toggle.accordion-2 variant="primary">Ставки</b-button>
+                </b-card-header>
+                <b-collapse id="accordion-2" role="tabpanel">                    
+                    <b-card-body>  
+                        <b-card-text class="left">Введите ставку дисконтирования, %:</b-card-text>                      
+                        <b-form-input v-model="discountRate" type="number" min="0" max="100"></b-form-input> 
+                        <b-form-checkbox id="check-reinvest" v-model="isReinvest" class="left" 
+                            switch>Реинвестируются ли доходы?</b-form-checkbox>                        
+                        <b-card-text class="left">Введите ставку реинвестирования, %:</b-card-text>
+                        <b-form-input v-model="reinvestRate" type="number" min="0" max="100"></b-form-input> 
                     </b-card-body>
                 </b-collapse>
             </b-card>
@@ -22,6 +38,10 @@ export default {
     data() {
         return {
             selectedTime: 'year',
+            numPeriods: 0,
+            discountRate: 0,
+            reinvestRate: 0,
+            isReinvest: false,
             times: [                
                 { value: 'year', text: 'год' },
                 { value: 'halfyear', text: 'полугодие' },
@@ -34,6 +54,7 @@ export default {
 </script>
 <style>
     .left {
+        margin-top: 10px;
         text-align: left;
     }
 </style>
