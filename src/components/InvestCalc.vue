9aa2907a67380053a4b45bc6ebfb991fef8b52b9
tretiakovoa <tretiakovoa@yandex.ru>
2019-10-29 12:34:51 +0300

    29-10-19 checkbox

--- src/components/InvestCalc.vue
+++ src/components/InvestCalc.vue
@@ -1,32 +1,36 @@
 <template>    
     <b-container>
-        <h1>Расчет эффективности инвестиций</h1>
-        <b-form @submit="onSubmit" @reset="onReset">
+        <h1>Расчет эффективности инвестиций</h1>        
+        <b-form>           
             <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block href="#" v-b-toggle.accordion-1 variant="primary">Периоды расчета</b-button>
+                    <b-button block v-b-toggle.accordion-1 variant="primary">Периоды расчета</b-button>
                 </b-card-header>
-                <b-collapse id="accordion-1" role="tabpanel" visible="true">                    
+                <b-collapse id="accordion-1" role="tabpanel" visible>                    
                     <b-card-body>  
                         <b-card-text class="left">Выберите продолжительность одного периода:</b-card-text>                      
                         <b-form-select v-model="selectedTime" :options="times"></b-form-select>                        
                         <b-card-text class="left">Введите общее количество периодов:</b-card-text>
-                        <b-form-input v-model="numPeriods" type="number" min="0" max="1000"></b-form-input>
+                        <b-form-input v-model="numPeriods" type="number" min="0" max="1000"></b-form-input>                         
                     </b-card-body>
                 </b-collapse>
             </b-card>
             <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block href="#" v-b-toggle.accordion-2 variant="primary">Ставки</b-button>
+                    <b-button block v-b-toggle.accordion-2 variant="primary">Ставки</b-button>                    
                 </b-card-header>
                 <b-collapse id="accordion-2" role="tabpanel">                    
-                    <b-card-body>  
+                    <b-card-body>    
                         <b-card-text class="left">Введите ставку дисконтирования, %:</b-card-text>                      
-                        <b-form-input v-model="discountRate" type="number" min="0" max="100"></b-form-input> 
-                        <b-form-checkbox id="check-reinvest" v-model="isReinvest" class="left" 
-                            switch>Реинвестируются ли доходы?</b-form-checkbox>                        
-                        <b-card-text class="left">Введите ставку реинвестирования, %:</b-card-text>
-                        <b-form-input v-model="reinvestRate" type="number" min="0" max="100"></b-form-input> 
+                        <b-form-input v-model="discountRate" type="number" min="0" max="100"></b-form-input>
+                        <br/>
+                        <b-form-checkbox id="check-reinvest" class="left" switch v-model="isReinvest">
+                            Реинвестируются ли доходы?</b-form-checkbox> 
+                        <!-- <label for="check-reinvest">{{ isReinvest }}</label> -->
+                        <b-collapse :visible="isReinvest">
+                            <b-card-text class="left">Введите ставку реинвестирования, %:</b-card-text>
+                            <b-form-input v-model="reinvestRate" type="number" min="0" max="100" :disabled="!isReinvest"></b-form-input>
+                        </b-collapse>                                                 
                     </b-card-body>
                 </b-collapse>
             </b-card>
@@ -37,17 +41,23 @@
 export default {
     data() {
         return {
+            isReinvest: false,
             selectedTime: 'year',
             numPeriods: 0,
             discountRate: 0,
-            reinvestRate: 0,
-            isReinvest: false,
+            reinvestRate: 0,  
             times: [                
                 { value: 'year', text: 'год' },
                 { value: 'halfyear', text: 'полугодие' },
                 { value: 'quarter', text: 'квартал' },
                 { value: 'month', text: 'месяц' }
             ]
+        }
+    },
+    methods: {
+        toggleReinvest() {
+            console.log('change reinvest')
+            this.isReinvest = !this.isReinvest;
         }
     }
 }
